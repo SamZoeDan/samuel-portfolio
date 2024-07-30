2243d5d108400126f9f8f5b808fc697766e765c5
SamZoeDan <samuelzoekasongo@gmail.com>
2024-07-30 18:51:51 -0400

    First Commit
    Project cleaning

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Recipe list</h1>
+      <h2>Recipe list</h2>
+      <h5>Recipe list</h5>
+
+      <ul>
+
+        <li>Element 1</li>
+        <li>Element 1</li>
+        <li>Element 1</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
